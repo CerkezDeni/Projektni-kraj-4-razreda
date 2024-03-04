6555d90a04b067332e60df9073797aca80c7e6d9
CerkezDeni <deni.cerkez@gmail.com>
2024-03-04 21:50:54 +0100

    Services Section 2

--- app.js
+++ app.js
@@ -6,5 +6,32 @@ menu.addEventListener('click', function() {
     menuLinks.classList.toggle('active');
 });
 
-var copy = document.querySelector(".logos-slide").cloneNode(true);
-document.querySelector('logos').appendChild(copy);
+// document.addEventListener("DOMContentLoaded", function() {
+//     var copy1 = document.querySelector(".logos-slide").cloneNode(true);
+//     document.querySelector(".logos").appendChild(copy1);
+
+//     var copy2 = document.querySelector(".logos-slide").cloneNode(true);
+//     document.querySelector(".logos").appendChild(copy2);
+// });
+
+
+document.getElementById("getStartedButton1").addEventListener("click", function() {
+    window.location.href = "services.html";
+});
+
+document.getElementById("getStartedButton2").addEventListener("click", function() {
+    window.location.href = "services.html";
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll(".services__card").forEach(function(card) {
+        card.addEventListener("click", function() {
+            // Determine which card was clicked based on its ID
+            if (card.id === "card1") {
+                window.location.href = "services.html";
+            } else if (card.id === "card2") {
+                window.location.href = "services.html";
+            }
+        });
+    });
+});
